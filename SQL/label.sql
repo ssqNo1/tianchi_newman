aab9e7cd56afa996ae17ada854fdce3137796e02
ssqNo1 <444751779@qq.com>
2016-05-24 21:30:36 +0800

    label bug fixed

--- SQL/label.sql
+++ SQL/label.sql
@@ -17,10 +17,9 @@ update object_day set obj = date(datetime(obj, '+1 day'));
 
 -- 将指定考察日的标签插入到标签表
 insert into label
-with u as (
+with u as ( -- 选取前一天的所有交互和前七天的非浏览交互
 	select user_id, item_id, time, behavior_type
-	from user, object_day
-	-- 选取前一天的所有交互和前七天的非浏览交互
+	from user, object_day	
 	where date(time) = date(datetime(object_day.obj, '-1 day'))
 	or (
 		date(time) <= date(datetime(object_day.obj, '-1 day')) and
@@ -28,10 +27,11 @@ with u as (
 		behavior_type != 1
 	)
 ),
-u4 as (
+u4 as ( -- 看考察日当天是否有购买行为
 	select user_id, item_id, 1 as 'buy'
-	from u
+	from user, object_day
 	where behavior_type = 4
+	and date(time) = date(datetime(object_day.obj))
 	group by 1,2
 	order by 1,2
 )
